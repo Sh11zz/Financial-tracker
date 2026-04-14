699814432b7e85ce9110281ecba314b2bbf83dd3
Sh11zz <artemklepi@gmail.com>
2026-04-14 23:39:38 +0300

    Добавил проект

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello world"
+
+
+@app.route("/api/users", methods=['GET'])
+def users():
+    return jsonify(
+        {
+            "Users": [
+                'user1',
+                'user2',
+                'user3'
+            ]
+        }
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port="8000")
